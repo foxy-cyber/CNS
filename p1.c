89f90a4f8d2b02dc649f9932f769788eb35e696e
Tilak <tilaksk2003@gmail.com>
2024-05-26 17:13:58 +0530

    -

--- p1.c
+++ p1.c
@@ -1,1 +1,24 @@
-#include
+#include <stdio.h>
+
+int main() {
+    int i=0;
+    char str[] = "Hello world";
+    printf("Original string: %s\n", str);
+    printf("AND with 127: ");
+    while(str[i] != '\0')
+        printf("%c", str[i++] & 127);
+    printf("\n");
+    printf("OR with 127: ");
+    i=0;
+    while(str[i] != '\0')
+        printf("%c", str[i++] | 127);
+    printf("\n");
+
+    printf("XOR with 127: ");
+    i=0;
+    while(str[i] != '\0')
+        printf("%c", str[i++] ^ 127);
+    printf("\n");
+
+    return 0;
+}
