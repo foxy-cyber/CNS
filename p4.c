72530af3f104bce5db18f188537566c6b644ea5d
Tilak <tilaksk2003@gmail.com>
2024-08-13 00:23:46 +0530

    -

--- p4.c
+++ p4.c
@@ -1,60 +1,136 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 
-void toUpperCase(char *str) {
-    for (int i = 0; i < strlen(str); i++) {
-        str[i] = toupper(str[i]);
+#define N 26
+
+int charToIndex(char c) {
+    return toupper(c) - 'A';
+}
+
+
+char indexToChar(int idx) {
+    return (idx % N) + 'A';
+}
+
+
+void matVecMult(int mat[][3], int vec[], int res[], int size) {
+    for (int i = 0; i < size; i++) {
+        res[i] = 0;
+        for (int j = 0; j < size; j++) {
+            res[i] = (res[i] + (mat[i][j] * vec[j])) % N;
+        }
+    }
+}
+
+void processMsg(int mode, int mat[][3], char msg[], int size) {
+    int len = strlen(msg);
+
+    for (int i = 0; i < len; i += size) {
+        int vec[3] = {0};
+        int res[3] = {0};
+
+        for (int j = 0; j < size; j++) {
+            if (i + j < len) {
+                vec[j] = charToIndex(msg[i + j]);
+            }
+        }
+
+        matVecMult(mat, vec, res, size);
+
+        for (int j = 0; j < size; j++) {
+            if (i + j < len) {
+                msg[i + j] = indexToChar(res[j]);
+            }
+        }
     }
 }
 
 int main() {
-    unsigned int encryptionMatrix[3][3] = {{6, 24, 1}, {13, 16, 10}, {20, 17, 15}};
-    unsigned int decryptionMatrix[3][3] = {{8, 5, 10}, {21, 8, 21}, {21, 12, 8}};
-    
-    unsigned int plaintextVector[3], ciphertextVector[3];
-    char plaintext[20];
-    int i, j, temp;
-    
-    printf("Enter the plaintext (3 characters): \n");
-    scanf("%s", plaintext);
+    int size, mat[3][3];
+    char msg[100], encMsg[100], decMsg[100];
 
-    toUpperCase(plaintext);
-    
-    if (strlen(plaintext) != 3) {
-        printf("Error: Plaintext must be exactly 3 characters long.\n");
-        return 1;
-    }
+    printf("Enter matrix size (2 or 3): ");
+    scanf("%d", &size);
 
-    for (i = 0; i < 3; i++) {
-        plaintextVector[i] = plaintext[i] - 'A';
-    }
-
-    for (i = 0; i < 3; i++) {
-        temp = 0;
-        for (j = 0; j < 3; j++) {
-            temp += encryptionMatrix[i][j] * plaintextVector[j];
+    printf("Enter key matrix elements:\n");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            scanf("%d", &mat[i][j]);
         }
-        ciphertextVector[i] = temp % 26;
     }
+    printf("Enter the message: ");
+    scanf("%s", msg);
+    strcpy(encMsg, msg);
+   
+    processMsg(1, mat, encMsg, size);
+    printf("Encrypted message: %s\n", encMsg);
 
-    printf("\nEncrypted ciphertext: ");
-    for (i = 0; i < 3; i++) {
-        printf("%c", ciphertextVector[i] + 'A');
-    }
 
-    for (i = 0; i < 3; i++) {
-        temp = 0;
-        for (j = 0; j < 3; j++) {
-            temp += decryptionMatrix[i][j] * ciphertextVector[j];
-        }
-        plaintextVector[i] = temp % 26;
-    }
-
-    printf("\nDecrypted plaintext: ");
-    for (i = 0; i < 3; i++) {
-        printf("%c", plaintextVector[i] + 'A');
-    }
+    printf("Decrypted message: %s\n", msg);
 
     return 0;
 }
+
+
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <ctype.h>
+
+// void toUpperCase(char *str) {
+//     for (int i = 0; i < strlen(str); i++) {
+//         str[i] = toupper(str[i]);
+//     }
+// }
+
+// int main() {
+//     unsigned int encryptionMatrix[3][3] = {{6, 24, 1}, {13, 16, 10}, {20, 17, 15}};
+//     unsigned int decryptionMatrix[3][3] = {{8, 5, 10}, {21, 8, 21}, {21, 12, 8}};
+    
+//     unsigned int plaintextVector[3], ciphertextVector[3];
+//     char plaintext[20];
+//     int i, j, temp;
+    
+//     printf("Enter the plaintext (3 characters): \n");
+//     scanf("%s", plaintext);
+
+//     toUpperCase(plaintext);
+    
+//     if (strlen(plaintext) != 3) {
+//         printf("Error: Plaintext must be exactly 3 characters long.\n");
+//         return 1;
+//     }
+
+//     for (i = 0; i < 3; i++) {
+//         plaintextVector[i] = plaintext[i] - 'A';
+//     }
+
+//     for (i = 0; i < 3; i++) {
+//         temp = 0;
+//         for (j = 0; j < 3; j++) {
+//             temp += encryptionMatrix[i][j] * plaintextVector[j];
+//         }
+//         ciphertextVector[i] = temp % 26;
+//     }
+
+//     printf("\nEncrypted ciphertext: ");
+//     for (i = 0; i < 3; i++) {
+//         printf("%c", ciphertextVector[i] + 'A');
+//     }
+
+//     for (i = 0; i < 3; i++) {
+//         temp = 0;
+//         for (j = 0; j < 3; j++) {
+//             temp += decryptionMatrix[i][j] * ciphertextVector[j];
+//         }
+//         plaintextVector[i] = temp % 26;
+//     }
+
+//     printf("\nDecrypted plaintext: ");
+//     for (i = 0; i < 3; i++) {
+//         printf("%c", plaintextVector[i] + 'A');
+//     }
+
+//     return 0;
+// }
--- p5.c
+++ p5.c
@@ -1,51 +1,108 @@
-#include <stdio.h>
-int IP[] = {2, 6, 3, 1, 4, 8, 5, 7};
-int IP_inverse[] = {4, 1, 3, 5, 7, 2, 8, 6};
-int S0[4][4] = {
-    {1, 0, 3, 2},
-    {3, 2, 1, 0},
-    {0, 2, 1, 3},
-    {3, 1, 3, 2}};
-int S1[4][4] = {
-    {0, 1, 2, 3},
-    {2, 0, 1, 3},
-    {3, 0, 1, 0},
-    {2, 1, 0, 3}};
-int initial_permutation(int plaintext)
-{
-    int result = 0;
-    for (int i = 0; i < 8; i++)
-    {
-        result |= ((plaintext >> (8 - IP[i])) & 1) << (7 - i);
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+int ip[] = {2, 6, 3, 1, 4, 8, 5, 7}; 
+int inverse[] = {4, 1, 3, 5, 7, 2, 8, 6}; 
+
+int initail(int pt) {
+    int res = 0;
+    for (int i = 0; i < 8; i++) {
+        res |= ((pt >> (8 - ip[i])) & 1) << (7 - i);
     }
-    return result;
+    return res;
 }
-int inverse_initial_permutation(int ciphertext)
-{
-    int result = 0;
-    for (int i = 0; i < 8; i++)
-    {
-        result |= ((ciphertext >> (8 - IP_inverse[i])) & 1) << (7 - i);
+
+int inve_initail(int ct) {
+    int res = 0;
+    for (int i = 0; i < 8; i++) {
+        res |= ((ct >> (8 - inverse[i])) & 1) << (7 - i);
     }
-    return result;
+    return res;
 }
-int s_box_substitution(int value, int s_box[4][4])
-{
-    int row = ((value & 0b1000) >> 2) | (value & 0b0001);
-    int col = (value & 0b0110) >> 1;
-    return s_box[row][col];
+
+void binary(int num) {
+    for (int i = 7; i >= 0; i--) {
+        printf("%d", (num >> i) & 1);
+    }
 }
-int main()
-{
-    int plaintext = 0b11010110;
-    printf("Plain Text: %x\n", plaintext);
-    int cipher_text = initial_permutation(plaintext);
-    printf("Cipher Text: %x\n", cipher_text);
-    // Example of S-box substitution
-    int s_box_value = 0b1101; // Example value
-    int s_box_result = s_box_substitution(s_box_value, S0);
-    printf("S-box result: %x\n", s_box_result);
-    int decrypted_text = inverse_initial_permutation(cipher_text);
-    printf("Decrypted Text: %x\n", decrypted_text);
+
+int main() {
+    char binaryStr[9];  
+    printf("Enter an 8-bit binary plaintext (e.g., 10010111): ");
+    scanf("%8s", binaryStr);  
+
+    int pt = 0;
+    for (int i = 0; i < 8; i++) {
+        pt = (pt << 1) | (binaryStr[i] - '0');  
+    }
+
+    printf("Plaintext is %x\n", pt);
+
+    int c = initail(pt);
+    int d = inve_initail(c);
+
+    printf("Cipher is %x\n", c);
+    printf("Cipher in binary is ");
+    binary(c);
+    printf("\n");
+
+    printf("Decrypted Plaintext is %x\n", d);
+    printf("Decrypted Plaintext in binary is ");
+    binary(d);
+    printf("\n");
+
     return 0;
 }
+
+// #include <stdio.h>
+// int IP[] = {2, 6, 3, 1, 4, 8, 5, 7};
+// int IP_inverse[] = {4, 1, 3, 5, 7, 2, 8, 6};
+// int S0[4][4] = {
+//     {1, 0, 3, 2},
+//     {3, 2, 1, 0},
+//     {0, 2, 1, 3},
+//     {3, 1, 3, 2}};
+// int S1[4][4] = {
+//     {0, 1, 2, 3},
+//     {2, 0, 1, 3},
+//     {3, 0, 1, 0},
+//     {2, 1, 0, 3}};
+// int initial_permutation(int plaintext)
+// {
+//     int result = 0;
+//     for (int i = 0; i < 8; i++)
+//     {
+//         result |= ((plaintext >> (8 - IP[i])) & 1) << (7 - i);
+//     }
+//     return result;
+// }
+// int inverse_initial_permutation(int ciphertext)
+// {
+//     int result = 0;
+//     for (int i = 0; i < 8; i++)
+//     {
+//         result |= ((ciphertext >> (8 - IP_inverse[i])) & 1) << (7 - i);
+//     }
+//     return result;
+// }
+// int s_box_substitution(int value, int s_box[4][4])
+// {
+//     int row = ((value & 0b1000) >> 2) | (value & 0b0001);
+//     int col = (value & 0b0110) >> 1;
+//     return s_box[row][col];
+// }
+// int main()
+// {
+//     int plaintext = 0b11010110;
+//     printf("Plain Text: %x\n", plaintext);
+//     int cipher_text = initial_permutation(plaintext);
+//     printf("Cipher Text: %x\n", cipher_text);
+//     // Example of S-box substitution
+//     int s_box_value = 0b1101; // Example value
+//     int s_box_result = s_box_substitution(s_box_value, S0);
+//     printf("S-box result: %x\n", s_box_result);
+//     int decrypted_text = inverse_initial_permutation(cipher_text);
+//     printf("Decrypted Text: %x\n", decrypted_text);
+//     return 0;
+// }
